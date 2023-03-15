e2bc1187fd3c12feda9ab122bfd36086dcfc69c2
LuciferHuang <huanglf01@mingyuanyun.com>
2023-03-15 19:13:12 +0800

    chore: change base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/',
+  base: 'heimdallr-sdk/',
   title: "HEIMDALLR-SDK",
   description: "A official document site of heimdallr-sdk",
   themeConfig: {
